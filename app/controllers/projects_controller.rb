06cd82cc9493f7468f16430587e12a8aafea2d4c
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-01-31 16:04:34 -0300

    head ok when update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,12 @@
 class ProjectsController < ApplicationController
   def index
-    set_projects
+    projects = Project.order(created_at: :asc)
+    render json: projects.all
   end
 
   def update
     project = Project.find(params[:id])
     project.update(completed_at: Time.now)
-    set_projects
-  end
-
-  protected
-
-  def set_projects
-    projects = Project.order(created_at: :asc)
-    render json: projects.all
+    head :ok
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,12 @@
 class TasksController < ApplicationController
   def index
-    set_tasks
+    tasks = Task.order(scheduled_at: :asc)
+    render json: tasks.all
   end
 
   def update
     task = Task.find(params[:id])
     task.update(completed_at: Time.now)
-    set_tasks
-  end
-
-  protected
-
-  def set_tasks
-    tasks = Task.order(scheduled_at: :asc)
-    render json: tasks.all
+    head :ok
   end
 end
