142986a48355365217b303c9a6d3de87e5a1a39e
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-05-18 19:06:41 -0300

    Update README with project information and installation guide

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_05_211514) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "users"
